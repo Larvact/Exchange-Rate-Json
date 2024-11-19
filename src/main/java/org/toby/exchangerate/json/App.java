6ebaa423afe884a702ae9db032edde1fa1c462a4
Larvact <tobyg7794@hotmail.com>
2024-11-19 12:04:21 +0000

    Initial project setup commit to hold json objects

---
+++ src/main/java/org/toby/exchangerate/json/App.java
@@ -0,0 +1,13 @@
+package org.toby.exchangerate.json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
