748d2c95acc8ce8cc16551738b169f61dcb1b85e
Larvact <tobyg7794@hotmail.com>
2024-11-21 18:49:55 +0000

    Added setter and no arg constructor to latest request class for json deserialisation

--- src/main/java/toby/exchangerate/json/api/exchangerates/latest/LatestCurrencyExchangeRatesRequest.java
+++ src/main/java/toby/exchangerate/json/api/exchangerates/latest/LatestCurrencyExchangeRatesRequest.java
@@ -3,11 +3,15 @@ package toby.exchangerate.json.api.exchangerates.latest;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class LatestCurrencyExchangeRatesRequest
 {
     @JsonProperty("base_currency_symbol")
